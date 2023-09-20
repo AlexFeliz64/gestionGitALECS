d72ddc2b29e14e5ed107f6ba237d1fe2744ff4ba
Alejandro <aalonsos21@iescastelar.com>
2023-09-20 18:07:15 +0200

    arreglo de bug

--- proyecto-java/src/App.java
+++ proyecto-java/src/App.java
@@ -3,5 +3,6 @@ public class App {
         System.out.println("Hello, World!");
         System.out.println("Hola mundo ");
         System.out.println("Hola Badajoz ");
+        System.out.println("Bug arreglado");
     }   
 }
